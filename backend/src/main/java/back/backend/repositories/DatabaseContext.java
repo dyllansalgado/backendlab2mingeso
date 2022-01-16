ac530773dbb2c2864588fa852835e6c06524458b
Miguel Díaz <miguel.diaz.ar@usach.cl>
2022-01-16 16:28:07 -0300

    a

--- backend/src/main/java/back/backend/repositories/DatabaseContext.java
+++ backend/src/main/java/back/backend/repositories/DatabaseContext.java
@@ -12,6 +12,6 @@ public class DatabaseContext {
         // Definir la URL de la BD, el usuario que la creó y la contraseña.
         // Ejemplo:jdbc:postgresql://127.0.0.1:5432/postgres, usuario, password
         public Sql2o sql2o(){
-            return new Sql2o("jdbc:postgresql://localhost:5432/db_tingeso", "postgres", "postgres");
+            return new Sql2o("jdbc:postgresql:///var/run/postgresql:5432/db_tingeso", "postgres", "postgres");
         }
 }
