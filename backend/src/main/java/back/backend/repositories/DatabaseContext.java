83ca3cf8d9e98d8f2b546b44c9ed6adf12a4e6f9
Miguel Díaz <miguel.diaz.ar@usach.cl>
2022-01-16 16:04:39 -0300

    p3

--- backend/src/main/java/back/backend/repositories/DatabaseContext.java
+++ backend/src/main/java/back/backend/repositories/DatabaseContext.java
@@ -7,17 +7,11 @@ import org.sql2o.Sql2o;
 
 @Configuration
 public class DatabaseContext {
-    @Value("${database.url}")
-    private String dbUrl;
-
-    @Value("${database.username}")
-    private String dbUser;
-
-    @Value("${database.password}")
-    private String dbPass;
     
     @Bean
-    public Sql2o sql2o(){
-        return new Sql2o(dbUrl, dbUser, dbPass);
-    }
+        // Definir la URL de la BD, el usuario que la creó y la contraseña.
+        // Ejemplo:jdbc:postgresql://127.0.0.1:5432/postgres, usuario, password
+        public Sql2o sql2o(){
+            return new Sql2o("jdbc:postgresql://localhost:5432/db_tingeso", "postgres", "postgres");
+        }
 }
