ad690afecdd549616bbff83e74672e591af776eb
Miguel Díaz <miguel.diaz.ar@usach.cl>
2022-01-16 14:42:55 -0300

    bd

--- basededatos/createDB.sql
+++ basededatos/createDB.sql
@@ -1,4 +1,19 @@
+DROP DATABASE db_tingeso;
+
+-- Creación de la base de datos.
+CREATE DATABASE db_tingeso 
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    TEMPLATE = template0
+    LC_COLLATE = 'es_CL.UTF-8' 
+    LC_CTYPE = 'es_CL.UTF-8';
+
+-- Conexión de la Base de datos.
+\connect db_tingeso;
+
 DROP TABLE IF EXISTS Producto CASCADE;
+
 CREATE TABLE Producto(
  id serial NOT NULL,
  codigoProducto text NOT NULL,
